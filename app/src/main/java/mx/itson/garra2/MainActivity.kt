647f65c3df7d258fd92bd0faf8e39ed5f80d841a
diego p <diegs311@gmail.com>
2025-03-07 09:57:19 -0700

    segunda fase proyecto

--- app/src/main/java/mx/itson/garra2/MainActivity.kt
+++ app/src/main/java/mx/itson/garra2/MainActivity.kt
@@ -1,13 +1,16 @@
 package mx.itson.garra2
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import mx.itson.garra.entities.Animal
 
-class MainActivity : AppCompatActivity() {
+    class MainActivity : AppCompatActivity(), View.OnLongClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,8 +20,27 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        Animal().save(this, "Tigre toñin")
-        Animal().get(this)
+        val btnNew = findViewById<Button>(R.id.btn_new)
+        btnNew.setOnClickListener(this)
+
+        val btnlist = findViewById<Button>(R.id.btn_list)
+        btnlist.setOnClickListener(this)
+
+        //Animal().save(this, "Tigre toñin")
+        //Animal().get(this)
 
     }
-}
+
+        override fun onLongClick(p0: View?): Boolean {
+            when (p0?.id){
+                R.id.btn_new ->{
+                    val interFormAnimal = Intent(this, AnimalFormActivity::class.java)
+                    startActivity(interFormAnimal)
+                }
+                R.id.btn_list ->{
+                    val interListAnimal = Intent(this, AnimalListActivity::class.java)
+                    startActivity(interListAnimal)
+                }
+            }
+        }
+    }
